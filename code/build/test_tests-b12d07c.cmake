73f8f789a2a2c268bf25e4cb119d49810fb29bec
Daniel Loch <dlloch2@illinois.edu>
2022-12-11 06:50:51 +0000

    Finished test cases and updated main.cpp

--- code/build/test_tests-b12d07c.cmake
+++ code/build/test_tests-b12d07c.cmake
@@ -4,4 +4,6 @@ add_test( test_is_valid /workspaces/cs225/release-f22/BitcoinTrustworthyTransact
 set_tests_properties( test_is_valid PROPERTIES WORKING_DIRECTORY /workspaces/cs225/release-f22/BitcoinTrustworthyTransactionProject/code/build)
 add_test( test_strongly_connected_components /workspaces/cs225/release-f22/BitcoinTrustworthyTransactionProject/code/build/test test_strongly_connected_components  )
 set_tests_properties( test_strongly_connected_components PROPERTIES WORKING_DIRECTORY /workspaces/cs225/release-f22/BitcoinTrustworthyTransactionProject/code/build)
-set( test_TESTS test_create_network test_is_valid test_strongly_connected_components)
+add_test( [==[test_dijkstra's - shortest path functions]==] /workspaces/cs225/release-f22/BitcoinTrustworthyTransactionProject/code/build/test [==[test_dijkstra's - shortest path functions]==]  )
+set_tests_properties( [==[test_dijkstra's - shortest path functions]==] PROPERTIES WORKING_DIRECTORY /workspaces/cs225/release-f22/BitcoinTrustworthyTransactionProject/code/build)
+set( test_TESTS test_create_network test_is_valid test_strongly_connected_components [==[test_dijkstra's - shortest path functions]==])
