b28f9630ec9ee9233b1116785003465221fa878d
Daniel Loch <dlloch2@illinois.edu>
2022-12-11 06:51:52 +0000

    Finished test cases and updated main.cpp

--- code/entry/main.cpp
+++ code/entry/main.cpp
@@ -12,7 +12,20 @@ int main(int argc, char* argv[]) {
     int startNode = stoi(argv[1]);
     int endNode = stoi(argv[2]);
     BitcoinNetwork b;
-    b.createNetwork("../../data/scc-complex.csv");
+    b.createNetwork("../../data/soc-sign-bitcoinotc.csv");
     cout << "Number of Strongly Connected Components: " << b.numberOfStronglyConnectedComponents() << endl;
+    if (!b.isValid(startNode, endNode)) {
+        cout << "No valid path between two users exists." << endl;
+    }
+    vector<int> path = b.findSingleSourceShortestPath(startNode, endNode);
+    cout << "Shortest path between two users: ";
+    for (size_t i = 0; i < path.size(); i++) {
+        if (i != path.size() - 1) {
+            cout << path[i] << " --> ";
+        } else {
+            cout << path[i] << endl;
+        }
+    }
+    cout << "Distance of shortest path between two users: " << b.findSingleSourceShortestPathDistance(startNode, endNode) << endl;
 }
 
--- code/tests/tests.cpp
+++ code/tests/tests.cpp
@@ -11,10 +11,13 @@
 TEST_CASE("test_create_network") {
     BitcoinNetwork b;
     BitcoinNetwork b1;
+    BitcoinNetwork b2;
     b.createNetwork("../../data/scc-simple.csv");
     b1.createNetwork("../../data/scc-complex.csv");
+    b2.createNetwork("../../data/soc-sign-bitcoinotc.csv");
     REQUIRE(b.getSize() == 5);
     REQUIRE(b1.getSize() == 12);
+    REQUIRE(b2.getSize() == 5881);
 }
 TEST_CASE("test_is_valid") {
     BitcoinNetwork b;
@@ -36,5 +39,20 @@ TEST_CASE("test_strongly_connected_components") {
     REQUIRE(b.numberOfStronglyConnectedComponents() == 3);
     REQUIRE(b1.numberOfStronglyConnectedComponents() == 4);
 }
-
-
+TEST_CASE("test_dijkstra's - shortest path functions") {
+    BitcoinNetwork b;
+    BitcoinNetwork b1;
+    b.createNetwork("../../data/scc-simple.csv");
+    b1.createNetwork("../../data/scc-complex.csv");
+    REQUIRE(b.findSingleSourceShortestPathDistance(2, 4) == 36);
+    REQUIRE(b.findSingleSourceShortestPathDistance(3, 1) == INT_MAX);
+    vector<int> v = {2, 1, 0, 3, 4};
+    REQUIRE(b.findSingleSourceShortestPath(2, 4) == v);
+    REQUIRE(b1.findSingleSourceShortestPathDistance(4, 7) == 8);
+    REQUIRE(b1.findSingleSourceShortestPathDistance(1, 12) == 53);
+    REQUIRE(b1.findSingleSourceShortestPathDistance(10, 4) == INT_MAX);
+    vector<int> v1 = {4, 5, 7};
+    vector<int> v2 = {1, 2, 4, 5, 7, 10, 11, 12};
+    REQUIRE(b1.findSingleSourceShortestPath(4, 7) == v1);
+    REQUIRE(b1.findSingleSourceShortestPath(1, 12) == v2);
+}
