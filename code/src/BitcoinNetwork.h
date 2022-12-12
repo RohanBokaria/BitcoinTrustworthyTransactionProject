3ee9018a10f163318b320d43ab57bdc78cecdccd
Siddharth Tangri <siddharth.tangri@gmail.com>
2022-12-12 18:24:23 +0000

    added time and space complexities to comments

--- code/src/BitcoinNetwork.h
+++ code/src/BitcoinNetwork.h
@@ -17,15 +17,30 @@ class BitcoinNetwork {
     public:
         // Reads in the csv file and populates the adjacency list with the information within the file
         void createNetwork(string fileName);
-        //return the number of nodes stored in the adjacency list
+        //Returns the number of nodes stored in the adjacency list
         size_t getSize();
-        // Uses Djikstra's algorithm to find the shortest distance (most trustworthy path) between the start and end node
+        /* 
+        Uses Djikstra's algorithm to find the shortest distance (most trustworthy path) between the start and end node
+        Time Complexity: O(ElogV)
+        Space Complexity: O(V)
+        */
         int findSingleSourceShortestPathDistance(int start, int end);
-        //Uses Djikstra's algorithm to find the shortest(most trustworthy) path between the start and end node
+        /*
+        Uses Djikstra's algorithm to find the shortest(most trustworthy) path between the start and end node
+        Time Complexity: O(ElogV)
+        Space Complexity: O(V)
+        */
         vector<int> findSingleSourceShortestPath(int start, int end);
-        //Uses Tarjan's algorithm to determine the number of strongly connected components in the bitcoin network
+        /*Uses Tarjan's algorithm to determine the number of strongly connected components in the bitcoin network
+        Time Complexity: O(V+E)
+        Space Complexity: O(V)
+        */
         int numberOfStronglyConnectedComponents();
-        // uses breadth first search to see if there is a path from the start node to the end node
+        /* 
+        uses breadth first search to see if there is a path from the start node to the end node
+        Time Complexity: O(V)
+        Space Complexity: O(V)
+        */
         bool isValid(int start, int end);
     private:
         //map storing the adjacency list of the bitcoin network
@@ -33,8 +48,12 @@ class BitcoinNetwork {
         vector<int> splitString(string row);
         //helper method used to find the shortest path 
         vector<unordered_map<int,int>> findSingleSourceShortestPathHelper(int start);
-        //dept first search helper Method used to find number of strongly connected components
+        //depth first search helper Method used to find number of strongly connected components
         void dfs(int node, stack<int>& st, unordered_map<int, bool>& onStackMap, unordered_map<int, int>& idMap, unordered_map<int, int>& lowMap, int& id, int& sccCount);
-        //used Bellman-Ford Algorithm to detect negative cycles, led us to change weights from -10 to 10, to 1 to 21 where 1 is the highest trustworthiness and 21 is the lowest trustworthiness
+        /* 
+        Used Bellman-Ford Algorithm to detect negative cycles, led us to change weights from -10 to 10, to 1 to 21 where 1 is the highest trustworthiness and 21 is the lowest trustworthiness
+        Time Complexity: O(VE)
+        Space Complexity: O(V)
+        */
         bool hasNegativeCycle();
 };
